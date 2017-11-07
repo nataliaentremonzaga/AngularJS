7185701f7a8291cb97bbb7bccffa03a0eca6fe89
Natalia <n.entremonzagamonje@gmail.com>
2017-11-07 09:46:29 +0100

    curso angularJS

---
+++ WebContent/Ejercicio06/js/app.js
@@ -0,0 +1,27 @@
+(function(){
+
+var app = angular.module('ejemplosApp',[ ]);
+
+
+
+app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
+
+
+  $scope.profesores={};
+
+  $http.get('json/profesores.json').success(function(data){
+
+    //codigo cuando es correcta la peticion
+    $scope.profesores= data.profesores;
+  });
+
+
+
+
+}]);
+
+
+
+
+
+})();
